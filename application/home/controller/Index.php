dd7cc871f1ece1891fb024805b2e834de2798cc5
xiangdehai <xiang_dehai@163.com>
2020-06-05 09:27:03 +0800

    modify

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -15,8 +15,10 @@ class Index extends Controller
 {
     public function index(){
 
-        $news = http_get('web/article/article_index');
+
+        echo 'test';exit;
+        /*$news = http_get('web/article/article_index');
         $this->assign('news', $news);
-        return $this->fetch();
+        return $this->fetch();*/
     }
 }
