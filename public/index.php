83be66ee59d257d29544de30b1cbf740ef23dabd
xiangdehai <xiang_dehai@163.com>
2020-06-05 11:15:10 +0800

    modify

--- public/index.php
+++ public/index.php
@@ -11,12 +11,10 @@
 
 // [ 应用入口文件 ]
 namespace think;
-
+exit('ffff');
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
 // 执行应用并响应
-
-exit('ff');
 Container::get('app')->run()->send();
